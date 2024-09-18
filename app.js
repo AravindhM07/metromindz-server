b3d43c706b85ac664c701ac006b3d2fcd6b1147c
AravindhM07 <115076285+AravindhM07@users.noreply.github.com>
2024-09-18 23:41:28 +0530

    Reduced the limit.

--- app.js
+++ app.js
@@ -14,9 +14,9 @@ connectDB();
 
 app.use(express.urlencoded({
     extended: true,
-    limit: '50mb'
+    limit: '5mb'
 }));
-app.use(express.json({ limit: '50mb' }));
+app.use(express.json({ limit: '5mb' }));
 app.use(cors({ origin: 'https://metromindz-client.vercel.app', credentials: true }));
 app.use(cookieParser());
 
